a6d33c5df78a4fb0aad73e1d55ef3c0684abbe91
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-12 12:57:26 +0000

    feat: add search books screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [state, setState] = useState("all");
   const [books, setBooks] = useState([]);
   const [featuredBook, setFeaturedBook] = useState({});
-  const [searchResults, setSearchResults] = useState({});
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -23,13 +23,14 @@ function App() {
   if (state == "all") page = <AllBooks books={books} />
   else if (state == "random") page = <RandomBook setFeaturedBook={setFeaturedBook} featuredBook={featuredBook} />
   else if (state == "add") page = <AddBook />
-  else if (state == "search") page = <FindBooks books={books} />
+  else if (state == "search") page = <FindBooks books={books} searchResults={searchResults} setSearchResults={setSearchResults}/>
 
   return (
     <>
       <div className="pageControls">
         <button onClick={()=>setState("all")}>See all books</button>
         <button onClick={()=>setState("random")}>Randomiser</button>
+        <button onClick={()=>setState("search")}>Find Books</button>
         {/* <button onClick={()=>setState("addBook")}>Add book</button> */}
       </div>
       {page}
@@ -81,8 +82,45 @@ const AddBook = ({setSearchResults, searchResults}) => {
 
 const FindBooks = ({setSearchResults, searchResults}) => {
 
+  const searchFunc = (e) => {
+    let mode = document.getElementById("searchMode");
+    mode = mode.options[mode.selectedIndex].value.toLowerCase();
+    console.log(mode);
+
+    let query = document.getElementById("searchInput").value;
+
+    console.log(mode, query);
+
+    (async () => {
+      const response = await fetch(`http://localhost:5001/books/${mode}/${query}`, {
+        method: "GET",
+        mode: "cors",
+      });
+
+      await setSearchResults(await response.json());
+
+      console.log(`http://localhost:5001/books/${mode}/${query}`, searchResults);
+    })();
+  }
+
   return (
-    <>
+    <>      
+      <div className="inputHolder">
+        <select id="searchMode">
+          <option>Title</option>
+          <option>Author</option>
+          <option>Genre</option>
+        </select>
+      <input id="searchInput"/></div>
+      <button onClick={searchFunc}>Search</button>
+
+      <div className="bookList">
+      {searchResults.map((item, index) => {
+        return (
+          <Book key={index} book={item} />
+        )
+      })}
+      </div>
     </>
   )
 }
